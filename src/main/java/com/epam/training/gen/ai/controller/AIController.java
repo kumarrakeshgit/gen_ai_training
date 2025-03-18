afb1b40de81788f9c49285ebe922fe6f1074251a
Rakesh Kumar1 <rakesh_kumar1@epam.com>
2025-03-18 15:29:44 +0530

    chat history implementation

--- src/main/java/com/epam/training/gen/ai/controller/AIController.java
+++ src/main/java/com/epam/training/gen/ai/controller/AIController.java
@@ -1,5 +1,6 @@
 package com.epam.training.gen.ai.controller;
 
+import com.epam.training.gen.ai.service.history.SimpleKernelHistory;
 import com.epam.training.gen.ai.service.promt.SimplePromptService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -10,9 +11,18 @@ public class AIController {
 
     @Autowired
     SimplePromptService simplePromptService;
+
+    @Autowired
+    SimpleKernelHistory simpleKernelHistory;
+
     @GetMapping("/query")
     public String getResponse(@RequestParam String input){
         return simplePromptService.getChatCompletionsHistory(input).toString();
+    }
+
+    @GetMapping("/context/query")
+    public String getResponseWithContext(@RequestParam String input){
+        return simpleKernelHistory.processWithHistory(input);
     }
 
 }
