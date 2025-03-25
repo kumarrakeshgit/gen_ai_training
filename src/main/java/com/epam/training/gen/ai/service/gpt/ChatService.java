245e9c9ebbc8530a12d276df01423f4569a110d9
Rakesh Kumar1 <rakesh_kumar1@epam.com>
2025-03-25 19:40:44 +0530

    update

--- src/main/java/com/epam/training/gen/ai/service/gpt/ChatService.java
+++ src/main/java/com/epam/training/gen/ai/service/gpt/ChatService.java
@@ -5,6 +5,11 @@ import com.azure.ai.openai.models.ChatCompletionsOptions;
 import com.azure.ai.openai.models.ChatRequestUserMessage;
 import com.epam.training.gen.ai.constants.ModelService;
 import com.epam.training.gen.ai.constants.OpenAiModel;
+import com.microsoft.semantickernel.Kernel;
+import com.microsoft.semantickernel.orchestration.InvocationContext;
+import com.microsoft.semantickernel.services.chatcompletion.ChatCompletionService;
+import com.microsoft.semantickernel.services.chatcompletion.ChatHistory;
+import com.microsoft.semantickernel.services.chatcompletion.ChatMessageContent;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -19,14 +24,23 @@ public class ChatService {
     @Autowired
     ModelService modelService;
 
+    @Autowired
+    ChatCompletionService chatCompletionService;
+
+    @Autowired
+    Kernel kernel;
+
+    @Autowired
+    InvocationContext invocationContext;
+
     public List<String> getChatCompletions(String prompt) {
-        ChatRequestUserMessage chatRequestUserMessage = new ChatRequestUserMessage(prompt);
-        ChatCompletionsOptions chatCompletionsOptions = new ChatCompletionsOptions(List.of(chatRequestUserMessage));
-        var completions = openAIAsyncClient
-                .getChatCompletions(modelService.selectModel(OpenAiModel.CHAT_COMPL_GPT_35), chatCompletionsOptions)
-                .block();
-        return completions != null ? completions.getChoices().stream()
-                .map(c -> c.getMessage().getContent())
-                .toList() : null;
+        ChatHistory history = new ChatHistory();
+        history.addUserMessage(prompt);
+
+        List<ChatMessageContent<?>> response = chatCompletionService.getChatMessageContentsAsync(
+                history, kernel, invocationContext).block();
+
+        history.addAll(response);
+        return response != null ? response.stream().map(ChatMessageContent::getContent).toList() : null;
     }
 }
